60777aa45b212614896e55d34c5f1e9ba1bc1ba0
Jigneshfp <pateljignesh511@gmail.com>
2018-04-30 15:41:25 +0530

    # added 3 line charts.

--- POS_new.R
+++ POS_new.R
@@ -32,4 +32,67 @@ POS_all$month_year <- factor(POS_all$month_year, levels = paste0(month.abb, "_",
 
 POS_all <- POS_all %>% arrange(month_year)
 
-#This is temp script.
+# summarising number of POS over months.
+POS_summary1 <- POS_all %>% gather(POS_type, Total_POS, c(POS_online, POS_offline)) %>% 
+  group_by(month_year, POS_type) %>% summarize(avg_POS = mean(Total_POS))
+
+# plot for average number of POS over months.
+g1 <- ggplot(POS_summary1, aes(x = month_year, y = avg_POS, col = POS_type, group = POS_type)) +
+  geom_line(size = 1) + geom_point(size = 1, shape = 16, fill = 1, color = 1)+
+  labs(title = "Monthly average number of POS deployed", 
+       subtitle = "2016 - 2017", 
+       x = "Month_year", 
+       y = "Avg. number of POS", 
+       caption = "RBI-database") + 
+  scale_color_discrete(name = "Type of POS", 
+                       labels = c("Offline","Online"), 
+                       guide = guide_legend(reverse = TRUE)) +
+  scale_y_continuous(breaks = seq(0, 60000, 10000), 
+                     labels = function(x) paste0(x/1000, "K")) +
+  theme(plot.title = element_text(hjust = 0.5), 
+        plot.subtitle = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+#summarising avg. amount of credid/debit over months.
+POS_summary2 <- POS_all %>% gather(Amount_type, Total_amount, c(amount_transc.debit, amount_transc.credit)) %>% 
+  group_by(month_year, Amount_type) %>% summarize(avg_amount = mean(Total_amount))
+
+# plot for avg. amount of credi/debit-card transaction over months.
+g2 <- ggplot(POS_summary2, aes(x = month_year, y = avg_amount, col = Amount_type, group = Amount_type)) +
+  geom_line(size = 1) + geom_point(size = 1, shape = 16, fill = 1, color = 1) +
+  labs(title = "Monthly average amount of transactions by debit/credit-card", 
+       subtitle = "2016 - 2017",
+       x = "Month_year",
+       y = "Avg. amount of transaction(Rs. Milions)", 
+       caption = "RBI-database") +
+  scale_y_continuous(breaks = seq(5000, 10000, 1000),labels =  function(x) {paste0(x/1000, "K")}) + 
+  scale_color_discrete(name = "Type of transaction",
+                       labels = c("By creditcard", "By debitcard"), 
+                       guide = guide_legend(reverse = TRUE)) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+#summarizing avg. number of transaction using credit/debit-card over months.
+POS_summary3 <- POS_all %>% gather(Transc_type, Total_no_transc, c(NO_of.transc.debit, NO_of.transc.credit)) %>%
+  group_by(month_year, Transc_type) %>% summarize(avg_NO.of.transc = mean(Total_no_transc))
+
+# plot for avg. number of transction using credit/debit-card over months.
+g3 <- ggplot(POS_summary3, aes(x = month_year, y = avg_NO.of.transc, col = Transc_type, group = Transc_type)) +
+  geom_line(size = 1) + geom_point(size = 1, shape = 16, fill = 1, color = 1) +
+  labs(title = "Monthly average number of transctions by credit/debit-card", 
+       subtitle = "2016 - 2017",
+       x = "Month_year",
+       y = "Avg. number of transaction", 
+       caption = "RBI-database") +
+  scale_y_continuous(breaks = seq(1000000, 8000000, 1000000),labels =  function(x) {paste0(x/1000, "K")}) + 
+  scale_color_discrete(name = "Type of transaction",
+                       labels = c("By creditcard", "By debitcard"), 
+                       guide = guide_legend(reverse = TRUE)) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+g1
+g2
+g3
